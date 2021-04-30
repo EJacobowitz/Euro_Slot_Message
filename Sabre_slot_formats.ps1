7d1dfd39a78858b6c82ba1ea46be89afbe5550f4
Jacobowitz, Eric <EJacobowitz@jetblue.com>
2021-04-30 12:08:42 -0400

    draft1

---
+++ Sabre_slot_formats.ps1
@@ -0,0 +1,135 @@
+
+function write-SAM {
+    [CmdletBinding()]
+    param (
+
+        [Parameter(Mandatory = $true)] $arcid, 
+        [Parameter(Mandatory = $true)] $ifplid,
+        [Parameter(Mandatory = $true)] $adep,
+        [Parameter(Mandatory = $true)] $ades,
+        [Parameter(Mandatory = $true)] $eobt,
+        [Parameter(Mandatory = $true)] $ctot,
+        [Parameter(Mandatory = $false)] $regul,
+        [Parameter(Mandatory = $false)] $taxitime,
+        [Parameter(Mandatory = $true)] $eobd,
+        [Parameter(Mandatory = $false)] $regcause,
+        [Parameter(Mandatory = $true)]$filepath
+        
+    )
+    
+    begin {
+        
+    }
+    
+    process {
+
+        $message = " 
+-TITLE SAM
+-ARCID $arcid
+-IFPLID $ifplid
+-ADEP $adep 
+-ADES $ades
+-EOBT $eobt
+-CTOT $ctot
+-REGUL $regul
+-TAXITIME $taxitime
+-EOBD $eobd
+-REGCAUSE $regcause
+"
+
+        $message | Out-File -FilePath $filepath -Force
+    }
+    
+    end {
+        
+    }
+}
+
+function write-SRM {
+    [CmdletBinding()]
+    param (
+
+        [Parameter(Mandatory = $true)] $arcid, 
+        [Parameter(Mandatory = $true)] $ifplid,
+        [Parameter(Mandatory = $true)] $adep,
+        [Parameter(Mandatory = $true)] $ades,
+        [Parameter(Mandatory = $true)] $eobt,
+        [Parameter(Mandatory = $true)] $ctot,
+        [Parameter(Mandatory = $false)] $regul,
+        [Parameter(Mandatory = $false)] $taxitime,
+        [Parameter(Mandatory = $true)] $eobd,
+        [Parameter(Mandatory = $false)] $regcause,
+        [Parameter(Mandatory = $true)]$filepath
+        
+    )
+    
+    begin {
+        
+    }
+    
+    process {
+
+        $message = " 
+-TITLE SRM
+-ARCID $arcid
+-IFPLID $ifplid
+-ADEP $adep 
+-ADES $ades
+-EOBD $eobd
+-EOBT $eobt
+-NEWCTOT $ctot
+-REGUL $regul
+-TAXITIME $taxitime
+-REGCAUSE $regcause
+"
+
+        $message | Out-File -FilePath $filepath -Force
+    }
+    
+    end {
+        
+    }
+}
+
+function write-SLC {
+    [CmdletBinding()]
+    param (
+
+        [Parameter(Mandatory = $true)] $arcid, 
+        [Parameter(Mandatory = $true)] $ifplid,
+        [Parameter(Mandatory = $true)] $adep,
+        [Parameter(Mandatory = $true)] $ades,
+        [Parameter(Mandatory = $true)] $eobt,
+        [Parameter(Mandatory = $false)] $regul,
+        [Parameter(Mandatory = $false)] $taxitime,
+        [Parameter(Mandatory = $true)] $eobd,
+        [Parameter(Mandatory = $false)] $regcause,
+        [Parameter(Mandatory = $true)]$filepath
+        
+    )
+    
+    begin {
+        
+    }
+    
+    process {
+
+        $message = " 
+-TITLE SLC
+-ARCID $arcid
+-IFPLID $ifplid
+-ADEP $adep 
+-ADES $ades
+-EOBD $eobd
+-EOBT $eobt
+-REGCAUSE $regcause
+-TAXITIME $taxitime
+"
+
+        $message | Out-File -FilePath $filepath -Force
+    }
+    
+    end {
+        
+    }
+}
